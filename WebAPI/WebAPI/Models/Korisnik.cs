ad932890ea9fd9196347d8c1d44f8686385725ab
Jelena96 <37114083+Jelena96@users.noreply.github.com>
2018-06-09 22:17:30 +0200

    klase- musterija i korisnik

---
+++ WebAPI/WebAPI/Models/Korisnik.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public abstract class Korisnik
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Jmbg { get; set; }
+        public string Email { get; set; }
+        public Pol PolKorisnika { get; set; }
+        public Uloga UlogaKorisnika { get; set; }
+    }
+}
---
+++ WebAPI/WebAPI/Scripts/login.js
@@ -0,0 +1,27 @@
+﻿
+$(document).ready(function () {
+
+    $('#registracijaForma').validate({ 
+        rules: {
+            korEmail: {
+                required: true,
+                email: true
+            },
+            korPas: {
+                required: true,
+                minlength: 5
+            },
+            korIme: {
+                required: true,
+                ime: true
+            }
+        }
+    });
+
+
+    $("#btnReg").click(function () {
+        alert("Text:");
+    });
+
+});
+
